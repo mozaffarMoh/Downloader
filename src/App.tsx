f27c2f03673384f750aa0bf03b7a7e81854821b6
Mozaffar <FERAWWWESS@gmail.com>
2024-04-16 09:59:31 +0300

    first-push

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Button, TextField } from "@mui/material";
 import "./App.scss";
-import Bill from "./components/Bill/Bill";
-import MenuList from "./components/MenuList/MenuList";
-
 function App() {
   return (
-    <div className="app">
-      <Bill />
-      <MenuList />
+    <div className="app flexCenterColumn">
+      <div className="container">
+        <h1>Downlaoder</h1>
+        <Button variant="contained" color="error">
+          Contained
+        </Button>
+
+        <TextField variant="outlined"/>
+      </div>
     </div>
   );
 }

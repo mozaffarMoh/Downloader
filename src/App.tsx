a4b2486efdc85d283a0e268a98bfd11d8ca7e21a
Mozaffar <FERAWWWESS@gmail.com>
2024-04-16 12:48:52 +0300

    handle-enter-key

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,95 @@
 import { Button, TextField } from "@mui/material";
 import "./App.scss";
+import { BiDownload } from "react-icons/bi";
+import { useForm } from "react-hook-form";
+import React from "react";
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  }: any = useForm();
+  const buttonRef: any = React.useRef(null);
+  const [link, setLink] = React.useState("");
+  const [success, setSuccess] = React.useState(false);
+
+  /* Check Link */
+  const handleCheckLink = () => {
+    setSuccess(true);
+  };
+
+  /* Empty the fields when success */
+  React.useEffect(() => {
+    if (success) {
+      setLink("");
+      setSuccess(false);
+    }
+  }, [success]);
+
+  /* Check link when press on Enter key */
+  React.useEffect(() => {
+    const handleEnterKey = (e: any) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        trigger("link").then((isValid: any) => {
+          if (isValid) {
+            handleSubmit(handleCheckLink)();
+          }
+        });
+        console.log(trigger);
+      }
+    };
+
+    document.addEventListener("keydown", handleEnterKey);
+
+    return () => {
+      document.removeEventListener("keydown", handleEnterKey);
+    };
+  }, [handleSubmit, trigger]);
+
   return (
     <div className="app flexCenterColumn">
-      <div className="container">
-        <h1>Downlaoder</h1>
-        <Button variant="contained" color="error">
-          Contained
+      <h1>Downlaoder</h1>
+      <div className="line"></div>
+      <h2>Free Online Video Downloader</h2>
+      <h3>Download videos for free || Youtube, Facebook, Instagram, etc...</h3>
+      <form
+        className="container flexCenterColumn"
+        onSubmit={handleSubmit(handleCheckLink)}
+      >
+        <TextField
+          variant="outlined"
+          color="error"
+          fullWidth
+          label="Paste your link here"
+          value={link}
+          {...register("link", {
+            required: "Please paste your link !!",
+            pattern: {
+              value:
+                /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/,
+              message: "Link should be a website link !!",
+            },
+          })}
+          onChange={(e: any) => setLink(e.target.value)}
+        />
+        {errors.link && (
+          <div className="error-message">{errors.link.message}</div>
+        )}
+        <Button
+          ref={buttonRef}
+          type="submit"
+          variant="contained"
+          fullWidth
+          color="error"
+          className="flexCenterColumn"
+        >
+          Download
+          <BiDownload size={25} />
         </Button>
-
-        <TextField variant="outlined"/>
-      </div>
+      </form>
     </div>
   );
 }
